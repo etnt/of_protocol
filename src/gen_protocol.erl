d177ae2198a90e3b6891f07659fe1209b7f22909
Torbjorn Tornkvist <tobbe@tail-f.com>
2013-10-10 14:07:21 +0200

    Reverting to old behaviour declaration

--- src/gen_protocol.erl
+++ src/gen_protocol.erl
@@ -7,12 +7,21 @@
 -module(gen_protocol).
 -author("Erlang Solutions Ltd. <openflow@erlang-solutions.com>").
 
-%% Encode OpenFlow Protocol message from Erlang representation to binary.
--callback encode(Message :: of_protocol:ofp_message()) ->
-    {ok, Binary :: binary()} |
-    {error, Reason :: any()}.
+-export([behaviour_info/1]).
 
-%% Decode OpenFlow Protocol message from binary to Erlang representation.
--callback decode(Binary :: binary()) ->
-    {ok, Message :: of_protocol:ofp_message(), binary()} |
-    {error, Reason :: any()}.
+%%% Requires too fancy Erlang version...
+
+%% Encode OpenFlow Protocol message from Erlang representation to binary.
+%-callback encode(Message :: of_protocol:ofp_message()) ->
+%    {ok, Binary :: binary()} |
+%    {error, Reason :: any()}.
+%
+%%% Decode OpenFlow Protocol message from binary to Erlang representation.
+%-callback decode(Binary :: binary()) ->
+%    {ok, Message :: of_protocol:ofp_message(), binary()} |
+%    {error, Reason :: any()}.
+
+%%% ...good'ol behaviour!
+
+behaviour_info(callbacks) -> [{encode,1}, {decode,1}];
+behaviour_info(_)         -> undefined.
